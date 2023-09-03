af3bd8491842165e087e283d96b1350853e85dc0
Skinick <107771280+Skinick@users.noreply.github.com>
2023-09-03 23:31:13 +0300

    LoginPage and LoginForm

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { signup } from './auth-operations';
+import { login, signup } from './auth-operations';
 
 const initialState = {
   user: {},
@@ -26,6 +26,21 @@ const authSlice = createSlice({
         state.isLogin = true;
       })
       .addCase(signup.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(login.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, { payload }) => {
+        const { user, token } = payload;
+        state.loading = false;
+        state.user = user;
+        state.token = token;
+        state.isLogin = true;
+      })
+      .addCase(login.rejected, (state, { payload }) => {
         state.loading = false;
         state.error = payload;
       });
