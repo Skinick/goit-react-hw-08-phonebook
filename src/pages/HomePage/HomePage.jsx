4ea36447503184212b057673135e8f68c24544b5
Skinick <107771280+Skinick@users.noreply.github.com>
2023-09-05 23:18:01 +0300

    css adaptive

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import Phone from '../HomePage/Images/phone.gif';
+import styles from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <main>
+      <section className={styles.container}>
+        <h1>Phonebook</h1>
+
+        <div className={styles.thumb}>
+          <div>
+            <img className={styles.image} src={Phone} alt="" />
+          </div>
+        </div>
+        <NavLink to="/contacts" className={styles.button} title="Try it now!">
+          Try it now!
+        </NavLink>
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
